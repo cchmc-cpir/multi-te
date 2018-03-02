70f6722e24491e527a073ac50ab3b92bde434b08
Alex Cochran <acochran50@gmail.com>
2018-03-02 17:41:33 -0500

    add respmode

--- reconstruction/multitesdc.m
+++ reconstruction/multitesdc.m
@@ -1,4 +1,4 @@
-function multitesdc(filename, numTE, numProj)
+function multitesdc(fidFile, numTE, numProj, numPoints, fidPoints, numPointsShift)
     %MULTITESDC Multi-TE sampling density compensation.
     %   Appropriately weights non-uniformly sampled k-space data to ensure accurate image
     %   reconstruction after interpolation onto a Cartesian grid. For use with data from interleaved
@@ -6,6 +6,14 @@ function multitesdc(filename, numTE, numProj)
     %
     %   Written by Jinbang Guo, Alex Cochran 2018.
     
+    
+    if strfind(fidFile, 'inspiration')
+        respMode = 'inspiration';
+    elseif strfind(fidFile, 'expiration')
+        respMode = 'expiration';
+    else
+        respMode = 
+    end
     
     
     
