645ed8b1e379b3c03273ff708ea383d1fc526518
Alex Cochran <acochran50@gmail.com>
2018-03-02 17:11:59 -0500

    create new multitesdc func draft

---
+++ reconstruction/multitesdc.m
@@ -0,0 +1,13 @@
+function multitesdc(filename, numTE, numProj)
+    %MULTITESDC Multi-TE sampling density compensation.
+    %   Appropriately weights non-uniformly sampled k-space data to ensure accurate image
+    %   reconstruction after interpolation onto a Cartesian grid. For use with data from interleaved
+    %   multi-TE UTE sequences.
+    %
+    %   Written by Jinbang Guo, Alex Cochran 2018.
+    
+    
+    
+    
+end
+
