0140fcdb3825cedd27019fd17d871c3ff4b1de9e
Alex Cochran <acochran50@gmail.com>
2018-03-23 22:01:39 -0400

    moved old files to archive

---
+++ +reconstruction/archive/grid3_multiTE.m
@@ -0,0 +1,119 @@
+%%Author: Jinbang Guo; image reconstruction for data from
+%%RetroGatingGenLeadPhase.m.
+function grid3_multiTE(NPoints,NPro,rampoints,fidpoints,LeadingCutPro,EndingCutPro,NPShift,RespMode,path,beta,NTE)
+
+% alpha=max([beta 1.375]); %gridding oversampling ratio.
+alpha=2;
+
+RealNPoints = NPoints - NPShift; % actual number of points encoded;
+RealNPro = NPro - LeadingCutPro - EndingCutPro;
+
+trajfilename = strcat('traj_',RespMode);
+DCFfilename = strcat('DCF_',RespMode,'.raw');
+fidname = strcat('fid_',RespMode);
+% imagefilename = strcat('img_TE','.raw');
+imagefilename = strcat('img_',RespMode,'.raw');
+
+% trajfilename = 'traj';
+% DCFfilename = 'DCF.raw';
+% fidname = 'fid';
+% imagefilename='img.raw';
+
+fid = fopen(fullfile(path,trajfilename));
+tmp = squeeze(fread(fid,inf,'double'));
+fclose(fid);
+tmp = reshape(tmp,[3,NPoints,NPro]);
+crds = tmp(:,1:RealNPoints,(LeadingCutPro+1):NPro-EndingCutPro);
+r = sqrt(crds(1,RealNPoints,:).^2 + crds(2,RealNPoints,:).^2 + crds(3,RealNPoints,:).^2);
+crds = crds./max(r(:))/2;
+clear tmp r;
+
+'   read pre-weights'
+fid = fopen(fullfile(path,DCFfilename));
+tmp = squeeze(fread(fid,inf,'float32'));
+fclose(fid);
+DCF = reshape(tmp,[RealNPoints,RealNPro]);
+clear tmp;
+
+% NTE=1;
+'   read k-space data'
+fid = fopen(fullfile(path,fidname));
+
+tmp = squeeze(fread(fid,inf,'int32'));%step-like scaling depending on SW_h.
+fclose(fid);
+% disp(size(tmp));disp(fidpoints);disp(NTE);disp(NPro);
+Alldata = reshape(tmp,[2,fidpoints,NTE,NPro]);
+clear tmp;
+
+
+
+for nte=1:NTE
+
+data = squeeze(Alldata(:,(NPShift +1):NPoints,nte,(LeadingCutPro+1):(NPro-EndingCutPro)));
+
+'   Grid3 params:'
+
+effMtx    = (RealNPoints - rampoints) * 2* alpha;
+% numThread = 8   % or 1 for no pthread lib exec
+numThread = 1;
+
+'   start Grid3 calc'
+gdata = grid3_MAT(data,crds,DCF,effMtx,numThread);
+clear data numThread;
+
+'   make a rolloff kernel'
+delta = [1.0, 0.0];
+k_not = [0.0, 0.0, 0.0];
+DCF_not   = 1.0;
+numThread = 1; % only have 1 data point
+rokern = grid3_MAT(delta',k_not',DCF_not,effMtx,numThread);
+
+clear delta k_not DCF_not numThread;
+
+'   fft into image space'
+% DATA
+% change to complex, fft, then shift
+gdata = squeeze(gdata(1,:,:,:) + 1j*gdata(2,:,:,:));
+gdata = fftn(gdata);
+gdata = fftshift(gdata,1);
+gdata = fftshift(gdata,2);
+gdata = fftshift(gdata,3);
+
+% ROLLOFF
+% change to complex, shift, then fft
+rokern = squeeze(rokern(1,:,:,:) + 1j*rokern(2,:,:,:));
+rokern = fftn(rokern);
+rokern = fftshift(rokern,1);
+rokern = fftshift(rokern,2);
+rokern = fftshift(rokern,3);
+rokern = abs(rokern);
+
+'   apply rolloff and crop'
+gdata(rokern > 0) = gdata(rokern > 0) ./ rokern(rokern > 0);
+xs = floor(effMtx/2 - effMtx/2/alpha)+1;
+xe = floor(effMtx/2 + effMtx/2/alpha);
+gdata = gdata(xs:xe,xs:xe,xs:xe);
+% img_r=real(gdata);
+% img_i=imag(gdata);
+% save(fullfile(path,strcat('image_TE',num2str(nte),'.mat')),'gdata');
+gdata = single(abs(gdata)); % magnitude, float32
+
+'   write output file'
+tmp = rot90(gdata,2);
+% fid = fopen(fullfile(path,strcat(imagefilename,num2str(nte))),'w');
+fid = fopen(fullfile(path,imagefilename),'w');
+fwrite(fid,tmp,'float32');
+fclose(fid);
+
+% fid=fopen(fullfile(path,strcat('image_r',num2str(nte),'.raw')),'w');
+% fwrite(fid,img_r,'float32');
+% fclose(fid);
+% fid=fopen(fullfile(path,strcat('image_i',num2str(nte),'.raw')),'w');
+% fwrite(fid,img_i,'float32');
+% fclose(fid);
+% end
+
+end
+
+end
+
---
+++ +reconstruction/archive/sdc_multiTE.m
@@ -0,0 +1,100 @@
+function sdc_multiTE(fileName,fileNumber,NTE,NPro)
+
+%%Author: Jinbang Guo; sampling density compensation for data from
+%%interleaved multi-TE UTE
+% clear all;
+%addpath '/users/frend7/recon/sdc3_nrz_11aug/';
+%addpath '/users/frend7/recon/grid3_dct_11aug/';
+%addpath '/users/frend7/recon/MultiTE_Recon/';
+% path = '/users/frend7/20160906_093109_IRC272A_MF_DoxyMice_1_1/';
+%path=strcat(['/users/frend7/',fileName]);
+% fileNumber = 16;
+% beta = 1.5; % expansion factor ratio: alpha_x/alpha_z, with alpha_x=alpha_y.
+beta=1;
+
+% NPro=ones(1,1);
+%NPro(1)=29556;
+
+
+% fid=fopen(char(strcat([path,'/',num2str(fileNumber),'/acqp'])));acqpRead=textscan(fid,'%s','delimiter','\n');acqpRead=acqpRead{1};
+% for index=1:size(acqpRead,1)
+% 	testStr=char(acqpRead{index});
+% 	if length(testStr)>10
+% 		if strcmp(testStr(1:11),'##$ACQ_size')==1
+% 			acqpReadFrames=str2num(acqpRead{index+1});
+% 		end
+% 	end
+% end
+% fclose(fid);
+% NPro=acqpReadFrames(2)/NTE;
+
+% NPro=15192; % overwrite readout to match gated number of projections
+
+NPoints=ones(1,1);
+fidpoints=ones(1,1);
+for i=1:1
+    NPoints(i)=101;
+    fidpoints(i)=128;
+end
+
+NPShift = 20; % number of points shifted before gradient on;
+rampoints=NPoints-84;
+
+LeadingCutPro=zeros(1,1);
+EndingCutPro=zeros(1,1);
+% for i=1:44
+% LeadingCutPro(i) =0;%number of leading spokes cut
+% EndingCutPro(i)= 0;%number of ending spokes cut;
+% end
+
+
+
+% respMode = 'measured2';%synthesized means no data registration, only traj synthesization for ellip traj.
+%RespMode='expiration';
+RespMode='inspiration';
+%RespMode='measured2';
+
+for i=1:numel(fileNumber)
+RealNPoints = NPoints(i) - NPShift;
+RealNPro = NPro(i) - LeadingCutPro(i)- EndingCutPro(i);
+realpath = fullfile(path,num2str(fileNumber(i)));
+trajfilename = strcat('traj_',RespMode);
+DCFfilename = strcat('DCF_',RespMode,'.raw');
+% trajfilename = 'traj';
+% DCFfilename = 'DCF.raw';
+
+% load coordinates
+ fid = fopen(fullfile(realpath,trajfilename));
+ tmp = squeeze(fread(fid,inf, 'double'));
+ fclose(fid);
+%disp(size(tmp));disp(NPoints(i));disp(NPro(i));
+ tmp = reshape(tmp,[3,NPoints(i),NPro(i)]);
+ crds = tmp(:,1:RealNPoints,(LeadingCutPro(i)+1):NPro(i)-EndingCutPro(i));%cut ending points along one spoke;
+ r = sqrt(crds(1,RealNPoints,:).^2 + crds(2,RealNPoints,:).^2 + crds(3,RealNPoints,:).^2);
+ crds = crds./max(r(:))/2;
+disp(['generating DCF for 'gr,realpath]);
+
+'   SDC params:'
+numIter = 25;
+effMtx  = (RealNPoints - rampoints(i)) * 2 * beta;
+osf     = 2.1;
+verbose = 1;
+
+'   start SDC calc'
+DCF = sdc3_MAT(crds,numIter,effMtx,verbose,osf);
+DCF = single(DCF); % float32
+
+'   write output DCF'
+tmp = DCF;
+fid = fopen(fullfile(realpath,DCFfilename),'w');
+    fwrite(fid,tmp,'float32');
+    fclose(fid);
+clear tmp DCF crds r trajfilename DCFfilename;
+
+
+disp(['Reconstructing ',realpath]);
+grid3_multiTE(NPoints(i),NPro(i),rampoints(i),fidpoints(i),LeadingCutPro(i),EndingCutPro(i),NPShift,RespMode,realpath,beta,NTE);
+
+end
+
+
